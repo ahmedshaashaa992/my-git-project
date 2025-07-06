061e6f5c7db3629f61c881b1c5953603a07297e4
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2025-07-06 14:46:34 +0200

    add color to Ahmed  text

--- frontend/src/Headers.jsx
+++ frontend/src/Headers.jsx
@@ -4,7 +4,7 @@ function Headers() {
   return (
     <>
      <div><h1>Welcome to Group A react platform</h1></div>
-        <div><h1>Teacher: Ahmed Shaashaa</h1></div>
+        <div><h1 style={{color:"red"}}>Teacher: Ahmed Shaashaa</h1></div>
         <div><h1>Student: Yasser Abdulmala</h1></div>
         <div><h1>Student: Reem Asaad</h1></div>
         <div><h1>Student: Reem Abdullah</h1></div>
