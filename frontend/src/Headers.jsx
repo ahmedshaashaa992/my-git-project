bcb4c227145b97397f2f0d0f9d16c0ce7a4eda8c
Alfares8 <mohammadkhairalfares@gmail.com>
2025-07-06 11:48:21 +0200

    mf

--- frontend/src/Headers.jsx
+++ frontend/src/Headers.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Headers() {
   return (
     <>
-     <div><h1>Welcome to Group A react platform</h1></div>
-        <div><h1>Teacher: Ahmed Shaashaa</h1></div>
+      <div><h1>Welcome to Group A react platform</h1></div>
+      <div><h1>Teacher: Ahmed Shaashaa</h1></div>
+      <div><h1>student: mohammad alfares</h1></div>
     </>
   )
 }
