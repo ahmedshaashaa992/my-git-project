10acdc337c1fa7d5e3a730883a8bcd494db35b08
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2025-07-06 12:40:09 +0200

    fix students errors

--- frontend/src/Headers.jsx
+++ frontend/src/Headers.jsx
@@ -8,9 +8,9 @@ function Headers() {
         <div><h1>Student: Yasser Abdulmala</h1></div>
         <div><h1>Student: Reem Asaad</h1></div>
         <div><h1>Student: Reem Abdullah</h1></div>
-        <div><h1>Mahdi AL Hariri</h1></div>
-        <div><h1> Teacher: Amina Alfadawiee</h1></div>
-        <div><h1>haitham almohammad</h1></div>
+        <div><h1>Student: Mahdi AL Hariri</h1></div>
+        <div><h1>Student: Amina Alfadawiee</h1></div>
+        <div><h1>Student: haitham almohammad</h1></div>
         <div><h1>student: mohammad alfares</h1></div>
     </>
 
