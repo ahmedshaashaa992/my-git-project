4e42211fd692ef197e879822145e16146d6a09be
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2025-07-10 21:06:06 +0200

    update yaml-8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-git-project/",
+  // base: "/my-git-project/",
+  base: process.env.BASE_URL || "/", // Dynamically set base
   plugins: [react()],
   build: {
     outDir: 'dist',
