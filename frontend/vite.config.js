3bfcf588ef8fe5382750b83369c80faa1847e857
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2025-07-10 20:17:09 +0200

    update vite-config2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-git-project/",
+  // base: "/my-git-project/",
   plugins: [react()],
   build: {
     outDir: 'dist',
